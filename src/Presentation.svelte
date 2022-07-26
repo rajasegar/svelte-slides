8864573c9300dc9f3020852be57a97858e326f16
Rajasegar <rajasegar.c@gmail.com>
2022-07-26 11:23:33 +0530

    chore: add examples from revealjs

--- src/Presentation.svelte
+++ src/Presentation.svelte
@@ -2,6 +2,10 @@
     import Title          from './slides/Title.svelte';
     import Love           from './slides/Love.svelte';
     import GettingStarted from './slides/GettingStarted.svelte';
+    import AutoAnimate from './slides/examples/AutoAnimate.svelte';
+    import Backgrounds from './slides/examples/Backgrounds.svelte';
+    import Barebones from './slides/examples/Barebones.svelte';
+    import LayoutHelpers from './slides/examples/LayoutHelpers.svelte';
 
     const partner = ['Svelte', 'Reveal.js'];
 </script>
@@ -9,4 +13,8 @@
 <Title/>
 <Love {partner}/>
 <GettingStarted/>
+<AutoAnimate/>
+<Backgrounds/>
+<Barebones/>
+<LayoutHelpers/>
 
---
+++ src/slides/examples/Barebones.svelte
@@ -0,0 +1,10 @@
+<section>
+  <h2>Barebones Presentation</h2>
+  <p>This example contains the bare minimum includes and markup required to run a reveal.js presentation.</p>
+</section>
+
+<section>
+  <h2>No Theme</h2>
+  <p>There's no theme included, so it will fall back on browser defaults.</p>
+</section>
+
